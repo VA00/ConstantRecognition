9915a0b5278329cde2e44e5c6baa5f1eb78a08ed
VA00 <aodrzywolek@gmail.com>
2023-12-16 15:58:26 +0100

    Added input number as commandline arg[1], added parser for complex numbers.

--- C/constantRecognition_batch.c
+++ C/constantRecognition_batch.c
@@ -13,6 +13,25 @@
 #include "math2.h"
 #include <time.h>
 
+double complex parseComplex(const char *str) {
+    double realPart, imagPart;
+    double complex result;
+
+    // Try reading as a complex number (real + imaginary)
+    if (sscanf(str, "%lf + %lfI", &realPart, &imagPart) == 2 ||
+        sscanf(str, "%lf - %lfI", &realPart, &imagPart) == 2) {
+        result = realPart + imagPart * I;
+    }
+    // Try reading as a purely real number
+    else if (sscanf(str, "%lf", &realPart) == 1) {
+        result = realPart + 0.0 * I;
+    } else {
+        // Return NaN + NaN * I to indicate error
+        result = NAN + NAN * I;
+    }
+
+    return result;
+}
 
 int main(int argc, char** argv)
 {
@@ -20,34 +39,36 @@ int main(int argc, char** argv)
   
   char amino[STACKSIZE];
   
-  double var, best;
+  double var, best, input_number;
   double complex computedX;
   
-  const double complex targetX=1.0175451652862630105139924352308+1;
+  //const double complex targetX=1.0+1.4634181403788164189078391170022I;
+
    
   int K, test, ULP;
   const int n=INSTR_NUM;
   int omp_cancel_flag=0, cpu_id=1, ncpus=1;
   
-  FILE  *flagfile;  
-  char buff[26];
+  FILE  *flagfile, *output;  
+  char str[123];
 
 
   if(!(argv[1]==NULL))
   {
-    sscanf(argv[1],"%d",&cpu_id);
-    sscanf(argv[2],"%d",&ncpus);
+    sscanf(argv[1],"%s", str);
+    sscanf(argv[2],"%d",&cpu_id);
+    sscanf(argv[3],"%d",&ncpus);
   }
 
-    //time_t now = time (0);
-    //strftime (buff, 26, "%Y-%m-%d %H:%M:%S.000", localtime (&now));
-    //printf ("%s\n", buff);
- 
-  setlinebuf(stdout); //disable 4kB stdout buffer
-  
-//  printf("Docelowa liczba = %.32lf + %.32lf i\n\n",creal(targetX), cimag(targetX));
-//  printf("cpu_id=%d\tncpus=%d\n",cpu_id,ncpus);
+  //printf("%s\n",str);
+  //exit(0);
 
+
+  double complex targetX = parseComplex(str);
+
+
+
+  setlinebuf(stdout); //disable 4kB stdout buffer
 
   best  = DBL_MAX;
   
@@ -104,7 +125,16 @@ int main(int argc, char** argv)
     if(var<best) 
      {
       best = var;
-      
+      ULP=0;
+      while( (computedX!=targetX) && abs(ULP) <1024*4 ){ ULP++; computedX=nextafter(computedX,targetX);}
+       
+      printf("%20llu\t%20llu\t%20llu\t%d\t%e\t%lf\t%lf\t%d\t",j,k1,k2,(ULP<1024*4) ? ULP : -1, best/DBL_EPSILON, creal(computedX),cimag(computedX),cpu_id);
+
+      char buff[26];
+      time_t now = time (0);
+      strftime (buff, 26, "%Y-%m-%d %H:%M:%S.000", localtime (&now));
+      printf ("%s\n", buff);
+
       //printf("\nBest_of_the_best from %d:\t%le\tj=%llu\tCODE:\t%s\n",
 	  //cpu_id, best/DBL_EPSILON, j,amino);
 	  //printf("Re=  %.32lf\t",creal(computedX));
@@ -117,10 +147,18 @@ int main(int argc, char** argv)
 	 if(best<=16*DBL_EPSILON) //jezeli znalazl, wychodzi z petli i zapisuje plik dla innych procesow
 	 {
 		  
-          printf("\nConstant recognized by thread %d:\t%le\tj=%llu\tCODE:\t%s\n",cpu_id, best/DBL_EPSILON,j,amino);
+          printf("\nConstant recognized by thread %d:\tError in $MachineEps=%le\tCode number=%llu\tSHORT CODE:\t%s\n",cpu_id, best/DBL_EPSILON,j,amino);
+
+          ULP=0;
+          while( (computedX!=targetX) && abs(ULP) <1024*4 ){ ULP++; computedX=nextafter(computedX,targetX);}
+            
+          printf("Total valid formulae [all codes] tested by thread %d:\t%llu [%llu]\n",cpu_id,k2,k1);
+          printf("Minimal error in ULP=%d\n", (ULP<1024*4) ? ULP : -1 );
+
 	      printf("Re=%.18lf\t",creal(computedX));
-	      printf("Im=%.18lf\t",cimag(computedX));
+	      printf("Im=%.18lf\t\n",cimag(computedX));
 	      
+          printf("RPN CODE:\t");
           print_code_mathematica(amino,K);
 	      printf("\n\n");
 
@@ -129,7 +167,7 @@ int main(int argc, char** argv)
           fprintf(flagfile, "%d", omp_cancel_flag);
           fclose(flagfile);
 
-		  break;
+		  exit(0);//break;
      }
 
     
