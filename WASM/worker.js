f5cf6dd2ba45295b3f45fa730b1e9322a446fb89
VA00 <aodrzywolek@gmail.com>
2024-02-08 16:29:12 +0100

    Added preliminary paralllel search version.

---
+++ WASM/worker.js
@@ -0,0 +1,27 @@
+// Worker script: worker.js
+importScripts('rpn_function.js');
+
+
+function doWork(initDelay, z, MaxCodeLength, cpuid, ncpus) {
+ 
+    return new Promise(resolve => {
+        setTimeout(() => { //Timeout is required for Module to initialize
+            
+            const result = Module.ccall('search_RPN', 'string', ['number', 'number', 'number', 'number'], [z, MaxCodeLength, cpuid, ncpus]);
+            
+            resolve(result);
+            
+        }, initDelay);
+    });
+}
+
+
+onmessage = async function(e) {
+    const { initDelay, z, MaxCodeLength, cpuId, ncpus } = e.data;
+    //console.log(`Worker (worker.js) ${cpuId} of ${ncpus} starting work for z=${z}, MaxCodeLength=${MaxCodeLength}`);
+    const resultString = await doWork(initDelay, z, MaxCodeLength, cpuId, ncpus);
+    
+    //console.log(`Worker (worker.js) ${cpuId} finished work with result ${resultString}`);
+
+    postMessage(`${resultString}`);
+};
