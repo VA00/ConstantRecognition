a6f23d745658c5f5ae6bbaad39b65409520a59cc
VA00 <aodrzywolek@gmail.com>
2024-02-18 18:40:53 +0100

    Added header for itoa_update

--- C/itoa.h
+++ C/itoa.h
@@ -1,4 +1,5 @@
 void itoa(unsigned long long int , char *, const int, const int);
+void itoa_update(unsigned long long int , char *, const int, const int);
 void itoaFAST(unsigned int , char *, unsigned const int, unsigned const int);
 int ilog(int , unsigned long long int );
 unsigned long long int ipow(int , int );
