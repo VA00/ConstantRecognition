813680bdf4a9b466837878d21fc03be3a1e29ece
VA00 <aodrzywolek@gmail.com>
2024-08-03 13:42:32 +0200

    Stored last working refactored web app.

--- WASM/worker.js
+++ WASM/worker.js
@@ -1,29 +1,25 @@
 // Worker script: worker.js
 importScripts('rpn_function.js');
 
-
-function doWork(initDelay, z, MaxCodeLength, cpuid, ncpus) {
- 
+function doWork(initDelay, z, MaxCodeLength, cpuId, ncpus) {
     return new Promise(resolve => {
-        setTimeout(() => { //Timeout is required for Module to initialize
-            
-            const result = Module.ccall('search_RPN', 'string', ['number', 'number', 'number', 'number'], [z, MaxCodeLength, cpuid, ncpus]);
-            
-            resolve(result);
-            
+        setTimeout(() => {
+            const result = Module.ccall('search_RPN', 'string', ['number', 'number', 'number', 'number'], [z, MaxCodeLength, cpuId, ncpus]);
+            resolve(JSON.parse(result));
         }, initDelay);
     });
 }
 
-
 onmessage = async function(e) {
     const { initDelay, z, MaxCodeLength, cpuId, ncpus } = e.data;
-    //console.log(`Worker (worker.js) ${cpuId} of ${ncpus} starting work for z=${z}, MaxCodeLength=${MaxCodeLength}`);
-    const resultString = await doWork(initDelay, z, MaxCodeLength, cpuId, ncpus);
-    //const resultArray = resultString.split(", ");
-    const resultJSON = JSON.parse(resultString);
-    //console.log(`Worker (worker.js) ${cpuId} finished work with result ${resultString}`);
-    //console.log(resultArray[resultArray.length-1]);
-
-    postMessage(resultJSON);
+    console.log(`Worker ${cpuId} of ${ncpus} starting work for z=${z}, MaxCodeLength=${MaxCodeLength}`);
+    
+    const resultJSON = await doWork(initDelay, z, MaxCodeLength, cpuId, ncpus);
+    
+    console.log(`Worker ${cpuId} finished work with result:`, resultJSON);
+    
+    postMessage({
+        cpuId,
+        ...resultJSON
+    });
 };
